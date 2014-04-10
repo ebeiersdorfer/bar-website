de8b9802b22485be6e388c444a19850d86cae438
joePasz <joseph.pasz@gmail.com>
2014-04-10 14:29:12 -0400

    Fixed search a little bit

--- bar-website-rails/app/controllers/main_controller.rb
+++ bar-website-rails/app/controllers/main_controller.rb
@@ -2,11 +2,8 @@ class MainController < ApplicationController
   def index
   	@bars = Bar.order(:name)
   	if params[:bar_search]
-  		@bars.keep_if {|v| v.name.include? params[:bar_search]}
+  		@bars.keep_if {|v| v.name.downcase.include? params[:bar_search].downcase}
   	end
   end
-  
-  def search
-  	@bars = Bar.order(:contact_number)
-  end
+
 end
