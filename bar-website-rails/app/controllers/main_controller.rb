3e96b48aa551ff540006630301ef70832fc1953e
joePasz <joseph.pasz@gmail.com>
2014-04-10 14:10:58 -0400

    Added crappy search functionality!

--- bar-website-rails/app/controllers/main_controller.rb
+++ bar-website-rails/app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController
   def index
   	@bars = Bar.order(:name)
+  	if params[:bar_search]
+  		@bars.keep_if {|v| v.name.include? params[:bar_search]}
+  	end
+  end
+  
+  def search
+  	@bars = Bar.order(:contact_number)
   end
 end
