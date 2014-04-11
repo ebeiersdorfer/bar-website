65f45884cdbed0548de2e750b2e0a583bc85c513
joePasz <joseph.pasz@gmail.com>
2014-04-11 15:27:52 -0400

    Added some comments!

--- bar-website-rails/app/controllers/main_controller.rb
+++ bar-website-rails/app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
   def index
+  	#This just pulls in all bars from the Bars Model, orders them, and stores them in this array
   	@bars = Bar.order(:name)
+  	#If this page was loaded with a search query, This just keeps all bars that contain the search string in them
   	if params[:bar_search]
   		@bars.keep_if {|v| v.name.downcase.include? params[:bar_search].downcase}
   	end
