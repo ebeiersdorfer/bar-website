5cded04d4d42b5444b107d1ed648a4c63980a2d8
joePasz <joseph.pasz@gmail.com>
2014-04-15 12:00:36 -0400

    bars are now in order based on location

--- bar-website-rails/app/controllers/application_controller.rb
+++ bar-website-rails/app/controllers/application_controller.rb
@@ -3,21 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
- def check_for_mobile
-    session[:mobile_override] = params[:mobile] if params[:mobile]
-    prepare_for_mobile if mobile_device?
-  end
-
-  def prepare_for_mobile
-    prepend_view_path Rails.root + 'app' + 'views_mobile'
-  end
-
-  def mobile_device?
-    if session[:mobile_override]
-      session[:mobile_override] == "1"
-    end
-  end
-  helper_method :mobile_device?
-  
-  before_filter :check_for_mobile
 end
